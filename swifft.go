78f52064284f53d7141619a4ac5f719098f984a1
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2024-09-09 00:58:27 +0700

    Add files via upload

---
+++ swifft.go
@@ -0,0 +1,56 @@
+package sphinxhash
+
+/*
+#cgo CFLAGS: -I/path/to/libswifft/include
+#cgo LDFLAGS: -L/path/to/libswifft/lib -lswifft
+#include "libswifft/swifft.h"
+#include <stdlib.h>
+*/
+import "C"
+import "unsafe"
+
+// AllocateInputBuffer allocates a buffer for SWIFFT input
+func AllocateInputBuffer() []byte {
+	size := C.SWIFFT_INPUT_BLOCK_SIZE
+	buffer := C.malloc(C.size_t(size))
+	if buffer == nil {
+		panic("failed to allocate input buffer")
+	}
+	return C.GoBytes(buffer, C.int(size))
+}
+
+// AllocateOutputBuffer allocates a buffer for SWIFFT output
+func AllocateOutputBuffer() []byte {
+	size := C.SWIFFT_OUTPUT_BLOCK_SIZE
+	buffer := C.malloc(C.size_t(size))
+	if buffer == nil {
+		panic("failed to allocate output buffer")
+	}
+	return C.GoBytes(buffer, C.int(size))
+}
+
+// AllocateCompactBuffer allocates a buffer for SWIFFT compact output
+func AllocateCompactBuffer() []byte {
+	size := C.SWIFFT_COMPACT_BLOCK_SIZE
+	buffer := C.malloc(C.size_t(size))
+	if buffer == nil {
+		panic("failed to allocate compact buffer")
+	}
+	return C.GoBytes(buffer, C.int(size))
+}
+
+// ComputeHash computes the SWIFFT hash
+func ComputeHash(input, output []byte) {
+	C.SWIFFT_Compute(
+		(*C.uchar)(unsafe.Pointer(&input[0])),
+		(*C.uchar)(unsafe.Pointer(&output[0])),
+	)
+}
+
+// CompactHash compacts the hash output
+func CompactHash(output, compact []byte) {
+	C.SWIFFT_Compact(
+		(*C.uchar)(unsafe.Pointer(&output[0])),
+		(*C.uchar)(unsafe.Pointer(&compact[0])),
+	)
+}
